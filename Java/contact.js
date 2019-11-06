57096d242cdac42ab9afb92eb677dc0d811a75af
BruceAmende <bruceamende32@gmail.com>
2019-11-06 10:48:52 +0100

    einde site

---
+++ Java/contact.js
@@ -0,0 +1,11 @@
+function groen(){
+    document.getElementById("body").style.backgroundColor= "green";
+    
+    
+    
+    
+}
+function rood(){
+    document.getElementById("body").style.backgroundColor= "red";
+   
+}
